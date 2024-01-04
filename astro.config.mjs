787185912cd26f5f2e19ffbe4427647d10d876ed
Nick Graffis <hi@nickgraffis.me>
2024-01-04 13:46:02 -0800

    a possible fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
+import { nodeExternals } from "rollup-plugin-node-externals";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  build: {
+    rollupOptions: {
+      plugins: [nodeExternals()],
+    },
+  },
 });
